bd40977acaeef796061156502b75f815326492b7
unknown <anamaitegarciacorrea@gmail.com>
2025-02-04 12:44:58 +0100

    Modifico una cosa de buscar prestamos

--- Vista/buscador_prestamos.php
+++ Vista/buscador_prestamos.php
@@ -14,8 +14,14 @@
             echo "<table><tr><th>Usuario</th><th>Amigo</th><th>Juego</th><th>Fecha</th><th>Devuelto</th></tr>";
             
             foreach ($resultadosBusqueda as $key => $value) {
-                //MODIFICA ESTO
-                echo "<tr><td>$value[0]</td><td>$value[1]</td><td>$value[2]</td><td>$value[3]</td></tr>";
+                $disponible;
+                if($value[4]==1){
+                    $disponible="SI";
+                }else{
+                    $disponible="NO";
+                }
+                
+                echo "<tr><td>$value[0]</td><td>$value[1]</td><td>$value[2]</td><td>$value[3]</td><td>$disponible</td></tr>";
             }
 
             echo "</table>";
